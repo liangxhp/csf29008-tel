a359b8c38dfc7a03d0a3448f29d8e1ad0d4ff2ba
aluno <aluno@labprog04.(none)>
2015-12-23 08:41:26 -0200

    Arrumando o Erlang C inverso.

--- erlangcinv.m
+++ erlangcinv.m
@@ -7,12 +7,21 @@ linha = [];
 coluna = [];
 [l,c] = size(b);
 n = [];
+aux = [];
+aux2 = 1;
 for i = 1:c
     if b(1,i) == delay
         for j = 2:l
-            if(b(j,i) == A)
+            if b(j,i) == A
                 n = j-1;
                 break;
+            else
+                aux(aux2) = A - b(j,i);
+                if(abs(aux(aux2)) < 0.5)
+                    n = j-1;
+                    break;
+                end
+                aux2 = aux2 + 1;
             end
         end
     end
